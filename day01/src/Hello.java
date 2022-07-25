78005d633ed6e44eafaa9c7a590899569982b2a0
user1 <user1@DESKTOP-EI3B66C>
2022-07-25 18:20:45 +0900

    day01 생성

---
+++ day01/src/Hello.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello java!");
+		System.out.println("Hello java!");
+		//System.out.println("Hello java!");
+		
+	}
+}
