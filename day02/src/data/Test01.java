1943672c69add186ab26767b92a44c302d40b76b
REDCRAB11 <hellow9596@naver.com>
2022-07-26 11:02:35 +0900

    따옴표의 의

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,16 @@
+package data; //가장 위에 현재 위치 표시하는 코드를 작성한다.
+
+import java.lang.*;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("2022-12-23"); //글자 취급 
+		System.out.println(2022-12-23); //계산식 취급 
+		System.out.println(2000);
+//		System.out.println(hello);  error 
+		
+
+	}
+
+}
