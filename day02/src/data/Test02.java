bf5aae0f04a375190930e54ca644fa89f1fb5672
REDCRAB11 <hellow9596@naver.com>
2022-07-26 11:33:58 +0900

    중국집 금액 계산

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,16 @@
+package data;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("자장면 5000");
+		System.out.println("짬뽕 6000");
+//		System.out.println("자장면 2그릇 + 짬뽕 3그릇");
+//		System.out.println(5);
+		System.out.println(5000*2 + 6000*3);
+		System.out.println(5000*2);
+		System.out.println(6000*3);
+		
+	}
+
+}
