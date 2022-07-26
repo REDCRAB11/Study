3816245bb74ea55b7495283eb24b66a999b48812
jiin_park <jiin_park@218.38.137.28>
2022-07-26 10:25:47 +0900

    설명추가

--- day01-1/src/Hello.java
+++ day01-1/src/Hello.java
@@ -1,11 +1,15 @@
+import java.lang.*; 
+// import 준비명령 + 오른쪽에 무엇을 준비할 것인지 씀  
+// * 와일드 카드 , 다 쓸 수 있음. 
+// 참고로 안써도 실행은 된다.
 
 public class Hello {
-
+ 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello java!");
 		System.out.println("Hello java!");
-		
+		// 출력 코드 - 결과화면에 원하는 글자를 출력 
 
 	}
 
