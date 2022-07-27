a6e36b60578a6e22639614478e6e645901c486fc
REDCRAB11 <hellow9596@naver.com>
2022-07-27 10:56:46 +0900

    문자(char)

--- day03-1/src/data/Test15.java
+++ day03-1/src/data/Test15.java
@@ -1,9 +1,26 @@
 package data;
-
+import java.lang.*;
 public class Test15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//문자(char,2byte)
+    // - 유니코드 한 글자 저장할 수 있는 형태
+	// - 외따옴표를 사용 
+	// - 문자열의 부품 역할을 수행 
+	// - 각각의 글자는 숫자 번호를 가지고 있다. 
+		
+		
+		char a = 'H';
+		System.out.println(a);
+		System.out.println((int)a);
+		
+		a += 32; //a를 32씩 증가시켜라(더해 넣어라)
+		System.out.println(a);
+		
+		
+		char b ='밥';
+		System.out.println(b);
+		
 
 	}
 
