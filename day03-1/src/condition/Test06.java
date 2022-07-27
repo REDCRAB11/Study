c0adf213aedbdac3a2e3ff02cc068af5c8067be8
REDCRAB11 <hellow9596@naver.com>
2022-07-27 18:02:27 +0900

    과속 단속 카메라 계산

--- day03-1/src/condition/Test06.java
+++ day03-1/src/condition/Test06.java
@@ -4,33 +4,38 @@ public class Test06 {
 
 	public static void main(String[] args) {
 		
-		int overkm = 50;
-		int overPrice = 10000;
-		int startPrice = 30000;
-		int car = 100; 
-		
-		int km = car - overkm; 
-		
-		int Price = startPrice + km / 10 * overPrice;
+		//준비 
+		int speed = 80; 
+		int limit = 50; 
 		
 		
 		
-		if(car > overkm ) { System.out.println("현재 주행 속도는:" + car + "입니다." + "제한속도를 초과하였습니다. 범칙금 "+ Price + "부과됩니다. ");
+		//계산 
+		boolean over = speed > limit;
 		
-		if(Price > 70000) { System.out.println("범칙금 지불하세요. ");
-		
-		}else {System.out.println("안전운전 하세요. ");
-		
+		int fine;
+		//if(over == true) {}  밑에거랑 같은 .. 
+		if(over) {
+			//int fine = 벌금 계산 결과; 
+			fine = 30000 +( speed - limit )/ 10 * 10000; 
+			if(fine >= 70000) { fine = 70000; 
+			
+			}else {
+				//할거 없음 없더도 되지만..
+			}
+		}
+		else {
+		     fine = 0;
+			
 		}
 		
 		
-//		if(Price < 70000) {System.out.println("범칙금은 7만원을 초과할 수 없습니다.");}
-//		if(Price > 70000) { System.out.println("범칙금 지불하세요. ");
-//		
-		}
+		
+		
+		//출력
+		
+		System.out.println("예상 벌금:" + fine + "원");
 
 	}
 
 }
-
-//7만원 초과할 수 없다를 어떻게 하냐고 ~ 
