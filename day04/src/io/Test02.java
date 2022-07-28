7946e7c2bb9b6ab4e2dbf5713d6f046076e62d4f
REDCRAB11 <hellow9596@naver.com>
2022-07-28 14:15:35 +0900

    system.in을 단독으로 사용하지 않는 이유

---
+++ day04/src/io/Test02.java
@@ -0,0 +1,18 @@
+package io;
+import java.io.IOException;
+import java.lang.*;
+public class Test02 {
+
+	public static void main(String[] args) throws IOException {
+		//system.in 만을 사용하여 입력을 받는 예제 근데 이거는 안씀 !!
+		//왜 안쓰냐?
+		// 1. 하나의 명령이 한 글자만 입력받는다. 
+		// 2. 입력을 아스키코드표 숫자로 변환해서 받는다.
+		// 3. 예외 처리를 해야한다. 
+		// 4. 자료형을 선택할 수 없다. 
+		
+		
+
+	}
+
+}
