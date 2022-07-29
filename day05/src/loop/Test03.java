9679ae06462655fcec123fe827217d5140e880d6
REDCRAB11 <hellow9596@naver.com>
2022-07-29 11:05:55 +0900

    for 반복의 범위와 반복수의 활용

---
+++ day05/src/loop/Test03.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		//반복수의 활용 
+		
+		//ex, 1부터 100까지 출력 
+		for(int i=1 ; i <=100 ; i++) {
+			System.out.println(i);
+		}
+		
+		//ex, 100부터 1까지 출력 
+		//for(int i=100; i>0 ; i=i-1) 가능 
+		for(int i=100 ; i > 0 ; i--) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
