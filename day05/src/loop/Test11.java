1918a28c266fde601232ca20c3cca8602cc0f0ab
REDCRAB11 <hellow9596@naver.com>
2022-07-29 16:52:42 +0900

    합계 구하기(total)

---
+++ day05/src/loop/Test11.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		
+		//합계 구하기 
+		
+		//ex, 1~10까지 더한 결과를 출력하세요. 
+		
+		
+		int total = 0;
+		for(int i=1; i<=10; i++) {
+			total += i;
+		}
+
+		System.out.println("Count: " + total);
+		
+
+	}
+
+}
