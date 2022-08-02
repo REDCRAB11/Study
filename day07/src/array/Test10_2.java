00053b06253d13f590a6df33015910b31b0f4657
REDCRAB11 <hellow9596@naver.com>
2022-08-02 17:29:43 +0900

    메뉴 랜점 정하기

--- day07/src/array/Test10_2.java
+++ day07/src/array/Test10_2.java
@@ -8,14 +8,15 @@ public class Test10_2 {
 	public static void main(String[] args) {
 		
 		//크키 설정 
-		int size =3; 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("메뉴개수: ");
+		int size = sc.nextInt(); 
 		
 		//배열 생성
 		String[]menu = new String[size];
 		//menu.length == size
 		
 		//입력
-		Scanner sc = new Scanner(System.in);
 		for(int i=0; i<size; i++) { //size번 반복문
 			System.out.print("메뉴 입력: ");
 			menu[i] =  sc.next(); //문자열은 next로만 받는다. 
@@ -25,7 +26,7 @@ public class Test10_2 {
 		//랜덤위치 생성 
 		Random r = new Random();
 		int index = r.nextInt(size);
-		System.out.println("index = " + index);
+//		System.out.println("index = " + index);
 		
 
 		//출력 
