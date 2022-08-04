cf3934ca90d82461fc0520418f493b3aa9fae669
REDCRAB11 <hellow9596@naver.com>
2022-08-04 14:14:26 +0900

    성적 계산 문제 풀이

---
+++ day09/src/oop/method3/Infor.java
@@ -0,0 +1,31 @@
+package oop.method3;
+
+public class Infor {
+	
+	String name;
+	int kor;
+	int eng;
+	int mat;
+	double total;
+	double avg;
+	
+	void set(String name, int kor, int eng, int mat) {//토탈과 평균은 넣을 필요 없다. 가지고 있는 정보로 만들 수 있기 떄문. 
+		
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		this.total = kor + eng + mat;
+		this.avg = this.total/3.0; 
+	}
+	
+	void vet() {
+		System.out.println("<학생정보>" + " \n" + "이름: " + name);
+		System.out.println("국어: " + kor + " 영어: " + eng + " 수학: " + mat);
+		System.out.println("총점: " + total + "점");
+		System.out.println("평균: " + avg + "점");
+	}
+	
+	
+
+}
---
+++ day09/src/oop/method3/Test01.java
@@ -0,0 +1,26 @@
+package oop.method3;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		
+		Infor a = new Infor();
+		Infor b = new Infor();
+		Infor c = new Infor();
+		
+		a.set("피카츄", 90, 90, 85);
+		a.vet();
+		
+		b.set("라이츄", 80, 90, 85);
+		b.vet();
+		
+		c.set("꼬부기", 100, 90, 90);
+		c.vet();
+		
+		
+		
+		
+	}
+
+}
