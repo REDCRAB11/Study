849af6d2ba12c9fb1093de7fa4101b8ba9cddaff
REDCRAB11 <hellow9596@naver.com>
2022-08-04 11:53:22 +0900

    메소드의 2가지 종류

---
+++ day09/src/oop/method1/Student.java
@@ -0,0 +1,32 @@
+package oop.method1;
+
+public class Student {
+	
+	// 멤버필드 : 클래스로 만든 객체의 구성요소, 데이터 저장소 
+	String name;
+	int score;
+	
+	// 멤버 메서드: 코드 저장소 
+	// - 메소드에서 this는 "자신"을 의미 
+	
+//	void 이름() {	
+//	코드
+//	}
+	
+	// 설정 메소드(코드 저장소)
+	//- 실행하려면 String과 int를 하나씰 전달해야하는 메소드 
+	void setting(String name, int score) {
+		this.name = name;
+		this.score = score;
+		
+	}
+	
+	
+	
+	//출력 메소드(코드 저장소)
+	void print() {
+		System.out.println("이름: " + this.name);
+		System.out.println("점수: " + this.score);
+		
+	}
+}
---
+++ day09/src/oop/method1/Test01.java
@@ -0,0 +1,25 @@
+package oop.method1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		Student a = new Student();
+		
+		a.setting("꼬북이", 90);
+		a.print();
+		
+		
+		Student b = new Student();
+		
+		b.setting("라이츄", 85);
+		b.print();
+		
+		Student c = new Student();
+		
+		c.setting("피카츄", 70);
+		c.print();
+		
+	}
+
+}
