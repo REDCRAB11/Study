b539fcea5868a0aefaa40f1de1207a7ffb04e282
REDCRAB11 <hellow9596@naver.com>
2022-08-08 15:59:27 +0900

    static 과 final 같이쓴다면

---
+++ day11/src/oop/keyword10/Test05.java
@@ -0,0 +1,19 @@
+package oop.keyword10;
+
+import java.util.Calendar;
+
+public class Test05 {
+	public static void main(String[] args) {
+		// JAVA의 상수 
+
+		System.out.println(Long.MAX_VALUE);	
+		System.out.println(Long.BYTES);
+		
+		System.out.println(Math.PI);
+		
+		System.out.println(Calendar.JANUARY);
+		
+		
+	}
+	
+}
---
+++ day11/src/oop/keyword10/Week.java
@@ -0,0 +1,7 @@
+package oop.keyword10;
+
+public class Week {
+	public static final String MONDAY = "월요일";
+	public static final String TUESDAY = "화요일";
+	// ..... 연속되게 쓰는 값들,, .. 을 묶어서 쓸때 편하게 쓸 수 있다. 
+}
