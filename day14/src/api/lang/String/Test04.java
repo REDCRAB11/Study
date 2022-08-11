7e74d20adb5c59724e56c81c4d1de0c7ec6c9ac8
REDCRAB11 <hellow9596@naver.com>
2022-08-11 14:14:27 +0900

    기타 문자열 명령 한글자씩, 글자자르기, 반복, 치환

---
+++ day14/src/api/lang/String/Test04.java
@@ -0,0 +1,35 @@
+package api.lang.String;
+
+public class Test04 {
+	public static void main(String[] args) {
+		//기타 문자열 명령 
+		String a = "안녕하세요";
+		
+		//1.한 글자씩 추출 명령
+		System.out.println(a.charAt(0));// a의 0번쨰 인덱스 글자 반환 
+		
+		// 반복문으로 한글자씩 출력
+		for(int i=0; i<a.length(); i++) {
+			char ch = a.charAt(i);
+			System.out.println(ch);
+		}
+		
+		//2. 글자 자르기 
+		System.out.println(a.substring(2));
+		System.out.println(a.substring(2,4));
+		
+		
+		//3. 반복 
+		String b = "똥";
+		System.out.println(b.repeat(2));
+		
+		
+		//4. 치환
+		String c = "나는 피자가 좋아";
+		System.out.println(c);
+		System.out.println(c.replace("피자", "자바"));
+		System.out.println(c.replace("피자", "**"));
+		
+		
+	}
+}
