4e43a06b9ffc6fbea57be7f2f1c300855b8897de
REDCRAB11 <hellow9596@naver.com>
2022-08-12 09:22:19 +0900

    욕 필터링 문제 풀이

--- day14/src/api/lang/String/Test06_1.java
+++ day14/src/api/lang/String/Test06_1.java
@@ -2,23 +2,12 @@ package api.lang.String;
 
 public class Test06_1 {
 	public static void main(String[] args) {
-		//준비
-		String [] filter = new String[] {
-				"신발", "개나리", "수박", "십장생", "시베리아", "주옥", "조카", "시방", "된장", "십자수"
-		};
-		
-		System.out.print("입력: ");
-		String text = " 이런 신발끈 개나리같은 수박씨를 보았다";
-		
-//		System.out.println(text);
-//		System.out.println(text.replace(filter[0], "***"));
-//		System.out.println(text.replace(filter[1], "***"));
-		
-		for(int i=0; i<filter.length; i++) {
-			text = text.replace(filter[i], "***");
+			
+			//준비
+			String text = "이런 신발끈 개나리같은 수박씨를 보았나";
+			
+			System.out.println(text);
+			System.out.println(text.replace("신발", "***"));
+			
 		}
-//		text = text.replace(filter[1], "***");
-		System.out.println(text);
-
-	}
 	}
--- day14/src/api/lang/String/Test06_5.java
+++ day14/src/api/lang/String/Test06_5.java
@@ -1,24 +1,19 @@
 package api.lang.String;
 
-public class Test06_3 {
+public class Test06_5 {
 	public static void main(String[] args) {
-		//준비
 		String [] filter = new String[] {
 				"신발", "개나리", "수박", "십장생", "시베리아", "주옥", "조카", "시방", "된장", "십자수"
 		};
 		
 		System.out.print("입력: ");
 		String text = " 이런 신발끈 개나리같은 수박씨를 보았다";
-		
-//		System.out.println(text);
-//		System.out.println(text.replace(filter[0], "***"));
-//		System.out.println(text.replace(filter[1], "***"));
+		String star = "*";
 		
 		for(int i=0; i<filter.length; i++) {
-			text = text.replace(filter[i], "***");
+			text = text.replace(filter[i], star.repeat(filter[i].length()));
 		}
-//		text = text.replace(filter[1], "***");
 		System.out.println(text);
-
+		
 	}
 }
