ad9f6c60d564732ce325a11810764dfb1486edbe
REDCRAB11 <hellow9596@naver.com>
2022-08-12 10:00:56 +0900

    day03_1 추가로 알아둘것

---
+++ day14/src/regex/Test03_1.java
@@ -0,0 +1,15 @@
+package regex;
+
+public class Test03_1 {
+	public static void main(String[] args) {
+		//유니코드표 안보고 한글 번호 맞추기 
+		System.out.println((int)'가');
+		System.out.println((int)'힣');
+		System.out.println((int)'ㄱ');
+		System.out.println((int)'ㅋ');
+		System.out.println((int)'ㅡ');
+		
+//		 자음:; ㄱ - ㅎ 모음:: ㅏ - ㅣ 글자:: 가 - 힣 
+		
+	}
+}
