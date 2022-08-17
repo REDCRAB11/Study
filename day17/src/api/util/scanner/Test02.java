fefcf824398a8146f560365c8720002f14642127
REDCRAB11 <hellow9596@naver.com>
2022-08-17 10:09:35 +0900

    nextline 줄바꿈 전까지 읽는 예제

---
+++ day17/src/api/util/scanner/Test02.java
@@ -0,0 +1,26 @@
+package api.util.scanner;
+
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {	
+		String origin = "학교종이 땡땡땡 어서모여라 \n 선생님이 우리를 기다리신다.";
+		Scanner sc = new Scanner(origin);
+		
+		// .nextLine(): 줄바꿈(\n) 전까지 읽는 명령 
+		// .hasNextLine() : 줄 읽기가 가능한지 확인하는 명령 
+//		
+//		System.out.println(sc.nextLine());
+//		System.out.println(sc.nextLine());
+//		System.out.println(sc.nextLine()); // 오류남 .. 
+// 		밑에처럼 줄여서 쓸 수 있따.! 		
+		
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		
+		sc.close();
+	}
+
+}
