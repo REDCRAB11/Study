9485520cd5d2ebdef7771fdad6444cac969851bf
REDCRAB11 <hellow9596@naver.com>
2022-08-17 12:01:06 +0900

    랜덤으로 list 뽑기

---
+++ day17/src/api/util/collection/Test04_1.java
@@ -0,0 +1,29 @@
+package api.util.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Test04_1 {
+	public static void main(String[] args) {
+		
+		// 저장소 생성 
+		//String [] list = new String[5];
+		List<String> list = new ArrayList<>();
+		
+		list.add("자바");
+		list.add("파이썬");
+		list.add("루비");
+		list.add("안드로이드");
+		list.add("자바스크립트");
+		
+//		System.out.println(list);
+		Random r = new Random();
+		int index = r.nextInt(list.size()); 
+		
+		System.out.println(list.get(index));
+		
+	
+	}
+
+}
