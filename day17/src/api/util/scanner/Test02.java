a171d0315fece24e3e6b56e1877f79ce454ba95c
REDCRAB11 <hellow9596@naver.com>
2022-08-17 10:15:47 +0900

    nextLine 엔터(줄바꿈)에 반응 예제

--- day17/src/api/util/scanner/Test02.java
+++ day17/src/api/util/scanner/Test02.java
@@ -8,7 +8,7 @@ public class Test02 {
 		String origin = "학교종이 땡땡땡 어서모여라 \n 선생님이 우리를 기다리신다.";
 		Scanner sc = new Scanner(origin);
 		
-		// .nextLine(): 줄바꿈(\n) 전까지 읽는 명령 
+		// .nextLine(): 줄바꿈(\n) 전까지 읽는 명령 (엔터에만 반응함!!!)
 		// .hasNextLine() : 줄 읽기가 가능한지 확인하는 명령 
 //		
 //		System.out.println(sc.nextLine());
