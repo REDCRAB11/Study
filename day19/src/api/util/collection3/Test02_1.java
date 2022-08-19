6b1e684786534f7e32cc3d4b7a2fb34826c84863
REDCRAB11 <hellow9596@naver.com>
2022-08-19 14:58:15 +0900

    Map 로그인 문제 풀이

---
+++ day19/src/api/util/collection3/Test02_1.java
@@ -0,0 +1,32 @@
+package api.util.collection3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Test02_1 {
+	public static void main(String[] args) {
+		// 저장소 생성 및 데이터 준비 	
+		Scanner sc = new Scanner(System.in);
+		Map<String, String> login = new HashMap<>();
+		
+		login.put("testuser", "testuser1234");
+		login.put("student", "student1234");
+		login.put("manager", "manager1234");
+		
+		System.out.print("ID: ");
+		String id = sc.next();
+		System.out.print("PW: ");
+		String pw = sc.next();
+		sc.close();
+
+		boolean isLogin = login.containsKey(id) && pw.equals(login.get(id));
+		
+		
+		if(isLogin) {
+			System.out.println("로그인 성공");
+		}else {
+			System.out.println("로그인 실패");
+		}
+	}
+}
