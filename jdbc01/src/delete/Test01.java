7e0b2aaca90f9df7261772b2a090503962b02c9f
REDCRAB11 <hellow9596@naver.com>
2022-08-25 16:08:16 +0900

    delete 문제풀이

---
+++ jdbc01/src/delete/Test01.java
@@ -0,0 +1,31 @@
+package delete;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import util.JdbcUtil;
+
+public class Test01 {
+	public static void main(String[] args) {
+		// 삭제(delete)
+		// delete fifa where state = ?
+		
+		// 데이터 준비 
+		String state = "스페인";
+		
+		//DB 처리 
+		JdbcTemplate template = JdbcUtil.getTemplate();
+		
+		String sql = "delete fifa where state =?";
+		Object [] param = {state};
+		
+		int result = template.update(sql,param);
+		
+		System.out.println("result: " + result);
+		if(result > 0) {
+			System.out.println("삭제가 완료되었습니다.");
+		}else {
+			System.out.println("존재하지 않는 나라 이름입니다. ");
+		}
+		
+	}
+}
---
+++ jdbc01/src/delete/Test02.java
@@ -0,0 +1,34 @@
+package delete;
+
+import java.util.Scanner;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import util.JdbcUtil;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("번호: ");
+		int no = sc.nextInt();
+		sc.close();
+		
+		JdbcTemplate template = JdbcUtil.getTemplate();
+		
+		String sql = "delete guest_book where no =?";
+//		System.out.println("입력:");
+//		Object [] param = { sc.next() }; 이렇게 사용하는 경우에는 복잡해진다 ~
+		Object [] param = {no};
+
+		
+		int result = template.update(sql,param);
+		
+		if(result > 0 ) {
+			System.out.println("삭제가 완료되었습니다.");
+		}else {
+			System.out.println("존재하지 않는 번호 입니다.");
+		}
+	}
+}
