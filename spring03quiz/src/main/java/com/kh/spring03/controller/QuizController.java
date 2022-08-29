ada603096d9aba1aa4e1799612b50552a61b0d41
REDCRAB11 <hellow9596@naver.com>
2022-08-29 17:50:11 +0900

    spring03 quiz 문제 풀이

--- spring03quiz/src/main/java/com/kh/spring03/controller/QuizController.java
+++ spring03quiz/src/main/java/com/kh/spring03/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.kh.spring03.controller;
 
+import java.text.DecimalFormat;
+import java.text.Format;
 import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
@@ -7,57 +9,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// 주소 전체에 물음표는 딱 하나만 가능 
 @Controller
+@RequestMapping("/quiz") // 공용 주소 
 public class QuizController {
 	
-	@RequestMapping("/bmi")
+	
+	@RequestMapping("/bmi") 
 	@ResponseBody
-	public String bmi(@RequestParam double height, double weight) {
-		double heightMeter = height/100;
-		double bmi = weight/(heightMeter*heightMeter);
-		return "BMI:" +bmi;
+	public String bmi(@RequestParam double cm,
+									@RequestParam double kg) {
+		double m = cm/100;
+		double bmi = kg/(m*m);
+		Format f = new DecimalFormat("#,##0.00");
+		return "BMI:" +f.format(bmi);
 	}
 	
 	
 	@RequestMapping("/subway")
 	@ResponseBody
 	public String subway(@RequestParam int year ) {
-		int birth = 20000101;
-		int years = birth/10000;
-		int nows = 2022; 
-		int ages = nows - years +1;
-		if(20 <= ages && ages < 65) {
-			System.out.println("요금: 1250");
-		//if(ages >= 20)
-		}else if(14 <= ages && ages < 20) {
-			System.out.println("요금: 720");
-		//if(ages >= 14)
-		}else if(8 <= ages && ages < 14) {
-			System.out.println("요금: 450원");
-		
+		int age = Calendar.getInstance().get(Calendar.YEAR) - year +1; 
+		int price;
+		if(20 <= age && age < 65) {
+			price = 1250;
+		}else if(14 <= age && age < 20) {
+			price = 720;
+		}else if(8 <= age && age < 14) {
+			price = 450;
 		}else{ 
-			System.out.println("요금: 0");
+			price =0;
 		}
-		return "요금: " + year;
+		return "요금: " + price;
 	}
 	
-	@RequestMapping("/calendar")
+	@RequestMapping("/calendar") // 실제주소 : /quiz/calendar
 	@ResponseBody
-	public String calendar(@RequestParam int year, int month) {
+	public String calendar(@RequestParam int year, 
+											@RequestParam int month) {
 		Calendar c = Calendar.getInstance();
 		c.set(year, month-1, 1); 
+
+		int max = c.getActualMaximum(Calendar.DATE);
 		
-		int week = c.get(Calendar.DAY_OF_WEEK);
-		
-		c.add(Calendar.DATE, -(week-1));  
-		
-		for(int i=0; i<42; i++) {
-			if(i % 7 ==6) {
-			}
-			c.add(Calendar.DATE, 1);
-		}
-		
-		return "요일: " + year + month;
+		return year + "년 " + month + "월" + max + "일까지 있습니다.";
 		
 		
 	}
