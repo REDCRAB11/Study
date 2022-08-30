2c1d41b01d94eb6fc8a8ca7a4f6aef2411964cb5
REDCRAB11 <hellow9596@naver.com>
2022-08-30 09:47:58 +0900

    spring04 quiz 문제 풀이

---
+++ spring04quize/src/main/java/com/kh/spring04/Spring04quizeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04quizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring04quizeApplication.class, args);
+	}
+
+}
