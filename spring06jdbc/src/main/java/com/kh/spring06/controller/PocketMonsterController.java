d3cc7202ee8eb5457f2492f9740032fe9e3f1836
REDCRAB11 <hellow9596@naver.com>
2022-08-31 10:01:11 +0900

    포멧몬스터 문제 풀이

--- spring06jdbc/src/main/java/com/kh/spring06/controller/PocketMonsterController.java
+++ spring06jdbc/src/main/java/com/kh/spring06/controller/PocketMonsterController.java
@@ -1,4 +1,5 @@
 package com.kh.spring06.controller;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -6,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kh.spring06.entity.PocketMonsterDto;
 
 //pocket_monster 테이블에 대한 CRUD처리를 위한 컨트롤러 
 @Controller
@@ -40,6 +43,50 @@ public class PocketMonsterController {
 			return "수정 완료.";
 		} else{
 			return "없는 번호.";
+		}
+	}
+	
+	//삭제 
+	@RequestMapping("/delete")
+	@ResponseBody
+	public String delete(@RequestParam int no) {
+		String sql = "delete pocket_monster where no =?";
+		Object[] param = {no};
+		int result = jdbcTemplate.update(sql,param);
+		if(result > 0) {
+			return "삭제 완료.";
+		}else {
+			return "없는 번호.";
+		}
+	}
+	
+	
+	// 목록 or 검색 -> RowMapper<PocketMonsterDto>
+	@RequestMapping("/select")
+	@ResponseBody
+	public String select() {
+		String sql= "select * from pocket_monster order by no asc";
+		List<PocketMonsterDto> list = jdbcTemplate.query(sql, PocketMonsterDto.getMapper());
+		StringBuffer buffer = new StringBuffer();
+		for(PocketMonsterDto dto : list) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return list.toString();
+	}
+	
+	
+	// 상세 -> ResultSetExtractor<PocketMonsterDto>
+	@RequestMapping("/detail")
+	@ResponseBody
+	public String detail(@RequestParam int no) {
+		String sql = "select * from pocket_monster where no=?";
+		Object[] param = {no};
+		PocketMonsterDto dto = jdbcTemplate.query(sql, PocketMonsterDto.getExtractor(),param);
+		if(dto == null) {
+			return "없는 번호";
+		}else {
+			return dto.toString();
 		}
 	}
 	
