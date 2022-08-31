1147999b57f5a3283e827eb44e044f99ebe4e7df
REDCRAB11 <hellow9596@naver.com>
2022-08-31 09:43:56 +0900

    spring07 문제풀이

---
+++ spring07quiz/src/main/java/com/kh/spring07/controller/GuestBookController.java
@@ -0,0 +1,191 @@
+package com.kh.spring07.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kh.spring07.entity.GuestBookDto;
+import com.kh.spring07.entity.GuestBookDto;
+
+@Controller
+@RequestMapping("guest-book")
+public class GuestBookController {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping("/insert")
+	@ResponseBody
+//	public String insert(@RequestParam int no, @RequestParam String name, @RequestParam String memo) {
+//		String sql = "insert into guest_book(no,name,memo) values(?,?,?)";
+//		Object [] param = {no, name, memo};
+//		jdbcTemplate.update(sql, param);
+//		return "등록완료";
+//	}
+	// 참고 @ModelAttribute는 객체의 필드에 자동 매핑을 수행한다. 
+	public String insert(@ModelAttribute GuestBookDto dto) {
+		String sql = "insert into guest_book(no,name,memo) values(guest_book_seq.nextval,?,?)";
+		Object[] param = {dto.getName(),dto.getMemo()};
+		jdbcTemplate.update(sql,param);
+		return"등록완료";
+	}
+	
+	@RequestMapping("/list")
+	@ResponseBody
+	public String list() {
+		String sql= "select * from guest_book order by no asc";
+		List<GuestBookDto> a = jdbcTemplate.query(sql, GuestBookDto.getMapper());
+		StringBuffer buffer = new StringBuffer();
+		for(GuestBookDto dto : a) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return a.toString();
+	}
+	
+//	사용자가 원하는 항목을 type이라는 이름으로 전송하면 해당 항목을 검색
+	@RequestMapping("/search")
+	@ResponseBody
+	public String search(
+			@RequestParam String type,
+			@RequestParam String keyword) {
+		if(type.equals("name")) {
+			String sql = "select * from guest_book where instr(name, ?) > 0 order by no asc";
+			Object[] param = {keyword};
+			List<GuestBookDto> data = 
+				jdbcTemplate.query(sql, GuestBookDto.getMapper(), param);
+			
+			StringBuffer buffer = new StringBuffer();
+			for(GuestBookDto dto : data) {
+				buffer.append(dto);
+				buffer.append("<br>");
+			}
+			return buffer.toString();
+		}
+		else if(type.equals("memo")) {
+			String sql = "select * from guest_book where instr(memo, ?) > 0 order by no asc";
+			Object[] param = {keyword};
+			List<GuestBookDto> data = 
+				jdbcTemplate.query(sql, GuestBookDto.getMapper(), param);
+			
+			StringBuffer buffer = new StringBuffer();
+			for(GuestBookDto dto : data) {
+				buffer.append(dto);
+				buffer.append("<br>");
+			}
+			return buffer.toString();
+		}
+		else {
+			return "잘못된 검색";
+		}
+	}
+	
+	@RequestMapping("/search2")
+	@ResponseBody
+	public String search2(
+			@RequestParam String type,
+			@RequestParam String keyword) {
+		String sql;
+		if(type.equals("name")) {
+			sql = "select * from guest_book where instr(name, ?) > 0 order by no asc";
+		}
+		else {
+			sql = "select * from guest_book where instr(memo, ?) > 0 order by no asc";
+		}
+		
+		Object[] param = {keyword};
+		List<GuestBookDto> data = 
+			jdbcTemplate.query(sql, GuestBookDto.getMapper(), param);
+		
+		StringBuffer buffer = new StringBuffer();
+		for(GuestBookDto dto : data) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	
+	@RequestMapping("/search3")
+	@ResponseBody
+	public String search3(
+			@RequestParam String type,
+			@RequestParam String keyword) {
+		String sql = "select * from guest_book "
+							+ "where instr("+type+", ?) > 0 order by no asc";
+		Object[] param = {keyword};
+		List<GuestBookDto> data = 
+			jdbcTemplate.query(sql, GuestBookDto.getMapper(), param);
+		
+		StringBuffer buffer = new StringBuffer();
+		for(GuestBookDto dto : data) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	
+	@RequestMapping("/search4")
+	@ResponseBody
+	public String search4(
+			@RequestParam String type,
+			@RequestParam String keyword) {
+		String sql = "select * from guest_book "
+							+ "where instr(#1, ?) > 0 order by no asc";
+		sql = sql.replace("#1", type);
+		Object[] param = {keyword};
+		List<GuestBookDto> data = 
+			jdbcTemplate.query(sql, GuestBookDto.getMapper(), param);
+		
+		StringBuffer buffer = new StringBuffer();
+		for(GuestBookDto dto : data) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	
+	@RequestMapping("/detail")
+	@ResponseBody
+	public String detail(@RequestParam int no) {
+		String sql = "select * from guest_book where no=?";
+		Object[] param = {no};
+		GuestBookDto dto = jdbcTemplate.query(sql, GuestBookDto.getExtractor(),param);
+		if(dto == null) {
+			return "없는 번호";
+		}else {
+			return dto.toString();
+		}
+	}
+	
+	@RequestMapping("/update")
+	@ResponseBody
+	public String update(@RequestParam int no, @RequestParam String name, @RequestParam String memo) {
+		String sql = "update guest_book set name=?, memo=? where no=?";
+		Object[] param = {name, memo, no};
+		int result=	jdbcTemplate.update(sql, param);
+		if( result > 0) {
+			return "수정 완료.";
+		} else{
+			return "없는 번호.";
+		}
+	}
+	
+	@RequestMapping("/delete")
+	@ResponseBody
+	public String delete(@RequestParam int no) {
+		String sql = "delete guest_book where no =?";
+		Object[] param = {no};
+		int result = jdbcTemplate.update(sql,param);
+		if(result > 0) {
+			return "삭제 완료.";
+		}else {
+			return "없는 번호.";
+		}
+	}
+}
